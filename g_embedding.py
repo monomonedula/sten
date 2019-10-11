ca4f9690726f538e4a66a55bbbf75de3ee0ccced
monomonedula <valh@tuta.io>
2019-10-11 18:10:40 +0300

    refactoring code to use matrices for computations

--- g_embedding.py
+++ g_embedding.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import sparse
 from scipy.sparse import csr_matrix
 from pypardiso import spsolve
 import networkx as net
@@ -36,23 +37,43 @@ class SystemLeft:
         self._coeff = coefficient
 
     def matrix(self):
-        if self._cached:
-            return self._cached
-        row_length = (self._graph.vertices_num * 2 + self._graph.nodes_num)
-        print(row_length)
-        left_data = np.empty([row_length], dtype=float)
-        left_indices = np.empty([row_length], dtype=int)
-        left_indptr = np.empty(len(self._graph) + 1, dtype=int)
-        left_indptr[0] = 0
-        counter = 0    
-        for row_node in range(len(self._graph)):
-            neighbors = self._graph[row_node]
-            for node in neighbors:
-                left_data[counter] = self._coeff(node, row_node)
-                left_indices[counter] = node
-                counter += 1
-            left_indptr[row_node + 1] = counter
-        return csr_matrix((left_data, left_indices, left_indptr), dtype=float)
+        if not self._cached:
+            row_length = (self._graph.vertices_num * 2 + self._graph.nodes_num)
+            print(row_length)
+            left_data = np.empty([row_length], dtype=float)
+            left_indices = np.empty([row_length], dtype=int)
+            left_indptr = np.empty(len(self._graph) + 1, dtype=int)
+            left_indptr[0] = 0
+            counter = 0
+            for row_node in range(len(self._graph)):
+                neighbors = self._graph[row_node]
+                for node in neighbors:
+                    left_data[counter] = self._coeff(node, row_node)
+                    left_indices[counter] = node
+                    counter += 1
+                left_indptr[row_node + 1] = counter
+            self._cached = csr_matrix((left_data, left_indices, left_indptr), dtype=float)
+        return self._cached
+
+
+class SysLeft:
+    def __init__(self, graph, coefficient):
+        self._graph = graph
+        self._cached = None
+        self._coeff = coefficient
+
+    def matrix(self, dfactor):
+        if not self._cached:
+            nodes_number = self._graph.nodes_num
+            ones = csr_matrix(np.ones([1, nodes_number]))
+            weights = ones.dot(self._graph.adjacency)
+            weights.data = weights.power(-1, np.float) * dfactor
+            weights = weights.toarray().reshape([nodes_number,])
+            weights = sparse.diags(weights).tocsr()
+            matrix = self._graph.adjacency.dot(weights)
+            matrix.setdiag(-1.0)
+            self._cached = matrix
+        return self._cached
 
 
 class SystemRight:
